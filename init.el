4f4c44518cd00e9d04f1360a871c184f8dd56d50
Dirk Hornung <dirkhornung91@gmail.com>
2019-09-30 18:04:16 +0200

    project compile and run keybindings

--- init.el
+++ init.el
@@ -34,7 +34,9 @@
  '(package-selected-packages
    '(clang-format yasnippet-snippets cmake-mode ccls magit platformio-mode helm-lsp lsp-ui company-lsp yasnippet flutter-l10n-flycheck flutter dart-mode lsp-mode use-package-ensure-system-package markdown-mode gist json-mode auctex-lua org-bullets typescript evil-collection neotree auctex-latexmk ranger evil-ranger zenburn-theme solarized-theme solarized-dark prettier-js prettier winum emacs-winum window-numbering evil-magit general which-key shackle helm-projectile exec-path-from-shell tide web-mode rjsx-mode use-package flycheck helm material-theme elpy evil-leader evil))
  '(safe-local-variable-values
-   '((projectile-project-test-cmd . "./build/runUnitTests")
+   '((projectile-project-run-cmd . "./main")
+     (projectile-project-compilation-cmd . "make main")
+     (projectile-project-test-cmd . "./build/runUnitTests")
      (projectile-project-run-cmd . "./build/FESR")
      (projectile-project-compilation-cmd . "make -C ./build -j8 default_target"))))
 (custom-set-faces
