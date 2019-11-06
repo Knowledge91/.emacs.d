2a936f2016540ddce716e65373f9527f105fed49
Dirk Hornung <dirkhornung91@gmail.com>
2019-11-06 14:34:58 +0100

    removed python + ein poly and added ein copy/yank keybindings

--- python/init.el
+++
@@ -1,6 +0,0 @@
-(use-package elpy ; python development environment
-  :ensure t
-  :config
-  (elpy-enable)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
