2a1c9cd69929c688cd29d467c437d1d5333160c8
Dirk Hornung <dirkhornung91@gmail.com>
2019-11-09 08:00:48 +0100

    command log mode

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  ;; If there is more than one, they won't work right.
  '(evil-collection-setup-minibuffer t)
  '(package-selected-packages
-   '(format-all format-all-the-code dap-lldb dap-mode pyvenv ein lsp-ui clang-format yasnippet-snippets cmake-mode ccls magit platformio-mode helm-lsp company-lsp yasnippet flutter-l10n-flycheck flutter dart-mode lsp-mode use-package-ensure-system-package markdown-mode gist json-mode auctex-lua org-bullets typescript evil-collection neotree auctex-latexmk ranger evil-ranger zenburn-theme solarized-theme solarized-dark prettier-js prettier winum emacs-winum window-numbering evil-magit general which-key shackle helm-projectile exec-path-from-shell tide web-mode rjsx-mode use-package flycheck helm material-theme elpy evil-leader evil))
+   '(command-log-mode format-all format-all-the-code dap-lldb dap-mode pyvenv ein lsp-ui clang-format yasnippet-snippets cmake-mode ccls magit platformio-mode helm-lsp company-lsp yasnippet flutter-l10n-flycheck flutter dart-mode lsp-mode use-package-ensure-system-package markdown-mode gist json-mode auctex-lua org-bullets typescript evil-collection neotree auctex-latexmk ranger evil-ranger zenburn-theme solarized-theme solarized-dark prettier-js prettier winum emacs-winum window-numbering evil-magit general which-key shackle helm-projectile exec-path-from-shell tide web-mode rjsx-mode use-package flycheck helm material-theme elpy evil-leader evil))
  '(safe-local-variable-values
    '((projectile-project-test-cmd . "./build/test")
      (projectile-project-run-cmd . "./build/main")
